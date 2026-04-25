933473a324c4213da9ac6eb4eb60fb935dfb3fe6
KHUSHI AGGARWAL <khushiaggarwal311@gmail.com>
2026-04-25 22:58:29 +0530

    Delete selection statementlab1(19/02/26) directory

--- selection statementlab1(19/02/26)/banking.py
+++
@@ -1,9 +0,0 @@
-transaction_amount = float(input("Enter transaction amount: "))
-account_age = int(input("Enter account age in months: "))
-transaction_type = input("Is the transaction international? (yes/no): ")
-
-# Condition check
-if transaction_amount > 200000 and account_age < 6 and transaction_type.lower() == "yes":
-    print("⚠ Transaction flagged for MANUAL VERIFICATION")
-else:
-    print("✅ Transaction is safe")
